5718e54016541279f90fcf08ec31f2b982e23c04
Tony Lockhart <tony.lockhart@ymail.com>
2024-10-27 16:31:24 -0400

    chore: Initial setup

---
+++ savings_account.py
@@ -0,0 +1,41 @@
+"""Import the Account class from the Account.py file."""
+import Account
+
+# Define a function for the Savings Account
+def create_savings_account(balance, interest_rate, months):
+    """Creates a savings account, calculates interest earned, and updates the account balance.
+
+    Args:
+        balance (float): The initial savings account balance.
+        interest_rate (float): The APR interest rate for the savings account.
+        months (int): The length of months to determine the amount of interest.
+
+    Returns:
+        float: The updated savings account balance after adding the interest earned.
+        And returns the interest earned.
+    """
+    # Create an instance of the `Account` class and pass in the balance and interest parameters.
+    #  Hint: You need to add the interest as a value, i.e, 0.
+    savings_account = Account.Account(balance, 0)
+    print("Balance:", savings_account.balance)
+    print("Interest rate:", savings_account.interest)
+    # Calculate interest earned
+    interest_earned = savings_account.balance * (interest_rate / 100 * months / 12)
+
+    # Update the savings account balance by adding the interest earned
+    updated_balance = savings_account.balance + interest_earned
+
+    # Pass the updated_balance to the set balance method using the instance of the SavingsAccount class.
+    savings_account.set_balance(updated_balance)
+
+    # Pass the interest_earned to the set interest method using the instance of the SavingsAccount class.
+    savings_account.set_interest(interest_earned)
+
+    # Return the updated balance and interest earned.
+    return  savings_account.balance, savings_account.interest
+
+
+if __name__ == "__main__":
+    # Call the run function.
+    balance, interest = create_savings_account(100, 5, 12)
+    print(f"Updated balance: {balance:.2f}, Interest earned: {interest:.2f}")
